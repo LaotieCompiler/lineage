43beae94dfd885324cc1f8a4bf32204a6cb2e46c
Zhijun <chenzj@yidoutang.com>
2024-05-11 00:29:09 +0800

    alias
    Signed-off-by: Zhijun <chenzj@yidoutang.com>

--- src/main/java/com/laotie/App.java
+++ src/main/java/com/laotie/App.java
@@ -1,4 +1,5 @@
 package com.laotie;
+
 import java.util.Set;
 
 import net.sf.jsqlparser.JSQLParserException;
@@ -17,35 +18,26 @@ import net.sf.jsqlparser.util.TablesNamesFinder;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     int tableNum = 0;
+
     public static void main(String[] args) {
-        // TODO: support: insert/insert overwrite/upsert/replace/update/merge/forien key
-        // String sqlStr = "-- insert into c1\r\n" + //
-        //                 "select * from(\r\n" + //
-        //                 "\tselect * from db1.a1 \r\n" + //
-        //                 "\tunion \r\n" + //
-        //                 "\tselect * from db1.a2\r\n" + //
-        //                 "\tunion \r\n" + //
-        //                 "\tselect * from db1.a3\r\n" + //
-        //                 ") aa left join db2.b1 as bb1 on a1.id = bb1.aid\r\n" + //
-        //                 "-- left join db2.b2 bb2 on a1.id = bb2.aid\r\n" + //
-        //                 "where bi.aid is not null;\r\n";
-        String sqlStr = "SELECT Col1, Col2, Col3  FROM"+// 
-                        "(SELECT A1 Col1,A2 Col2,A3 Col3 FROM ("+ //
-                            "SELECT B1 A1,B2 A2,B3 A3 FROM TB1 "+ //
-                        ")TB ) TA1 WHERE condition;";
+        String sqlStr = "SELECT Col1, Col2, Col3  FROM(" + //
+                            "SELECT A1 Col1,A2 Col2,A3 Col3 FROM (" + //
+                                "SELECT B1 A1,B2 A2,B3 A3 FROM TB1 " + //
+                            ") TB " + //
+                        ") WHERE condition;";
 
         Statement statHandle;
         Select select;
-        Insert insert;
         App app = new App();
         try {
             statHandle = (Statement) CCJSqlParserUtil.parse(sqlStr);
-            if (statHandle instanceof Select){
+            if (statHandle instanceof Select) {
                 select = (Select) statHandle;
-                app.parseLineage((PlainSelect) select);
+                String alias = String.format("table%d", app.getTableNum());
+                app.incrementTableNum();
+                app.parseLineage((PlainSelect) select,alias);
             }
             System.out.println("well done.");
         } catch (JSQLParserException e) {
@@ -53,23 +45,49 @@ public class App
         }
     }
 
+    public int getTableNum(){
+        return tableNum;
+    }
 
+    public void incrementTableNum() {
+        this.tableNum++;
+    }
 
-    private void parseLineage(PlainSelect select){
+    /**
+     * 查询 SELECT 语句的血缘信息
+     * 
+     * 采用后根法遍历语法树，先处理内层 FROM 语句（子节点），再处理 SELECT 语句（根节点）
+     */
+    private void parseLineage(PlainSelect select, String targetAlias) {
         FromItem fromItem = select.getFromItem();
 
+        String fromAlias = fromItem.toString();
+        if (fromItem.getAlias() != null) {
+            fromAlias = fromItem.getAlias().getName();
+        }else if (fromItem instanceof Table){
+            fromAlias = ((Table) fromItem).getName();
+        }else{
+            fromAlias = String.format("table%d", getTableNum());
+            incrementTableNum();
+        }
+
+        // 处理子节点：FROM 子查询。(TODO: Join 子查询)
         if (fromItem instanceof Select) {
             ParenthesedSelect parenthesedSelect = (ParenthesedSelect) fromItem;
-            parseLineage((PlainSelect)parenthesedSelect.getSelect());
+            parseLineage((PlainSelect) parenthesedSelect.getSelect(), fromAlias);
         }
-        String tableName = fromItem.toString();
-        if (null!=fromItem.getAlias()){
-            System.out.println("alias: table"+String.valueOf(tableNum)+" : "+fromItem.getAlias());
-            tableName = fromItem.getAlias().getName();
-            tableNum++;
-        }
-        for(SelectItem col: select.getSelectItems()){
-            System.out.println("from: "+ tableName + "." + col.getExpression().toString() + " to: table" + String.valueOf(tableNum)+"."+(null == col.getAlias()?col.getExpression().toString():col.getAlias()));
+
+        // String tableName = fromItem.toString();
+        // if (null != fromItem.getAlias()) {
+        //     System.out.println("alias: table" + String.valueOf(tableNum) + " : " + fromItem.getAlias());
+        //     tableName = fromItem.getAlias().getName();
+        //     tableNum++;
+        // }
+
+        for (SelectItem col : select.getSelectItems()) {
+            System.out.println("from: " + fromAlias + "." + col.getExpression().toString() + " to: "
+                    + targetAlias + "."
+                    + (null == col.getAlias() ? col.getExpression().toString() : col.getAlias()));
         }
     }
 

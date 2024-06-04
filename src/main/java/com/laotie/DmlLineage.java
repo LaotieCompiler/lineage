02d09bb3e37c47d2c43814bbf6d4357a49653ccf
Zhijun <chenzhijun@fongwell.com>
2024-06-04 16:29:38 +0800

    modify comment

--- src/main/java/com/laotie/DmlLineage.java
+++ src/main/java/com/laotie/DmlLineage.java
@@ -1,10 +1,6 @@
 /*-
- * #%L
- * JSQLParser library
- * %%
- * Copyright (C) 2004 - 2019 JSQLParser
- * %%
- * Dual licensed under GNU LGPL 2.1 or Apache License 2.0
+ * Author: Laotie
+ * Licensed under Apache License 2.0
  * #L%
  */
 package com.laotie;
